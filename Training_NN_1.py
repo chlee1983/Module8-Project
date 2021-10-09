733f804b3831e9657c044e90389e3573e8770200
chlee1983 <choonghong.lee@gmail.com>
2021-10-09 16:19:48 +0800

    "trainingNNoct9"

--- Training_NN_1.py
+++ Training_NN_1.py
@@ -10,7 +10,6 @@ import torchvision.models as models
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-
 # importing dataset
 ori_data_folder = './label'
 train_data_folder = './train'
@@ -22,23 +21,24 @@ train_dataset = torchvision.datasets.ImageFolder(root=train_data_folder, transfo
 original_dataset = torchvision.datasets.ImageFolder(root=ori_data_folder, transform=train_transforms)
 val_dataset = torchvision.datasets.ImageFolder(root=val_data_folder, transform=train_transforms)
 
-'''To show the images loaded from the ImageFolder above
-def show_transformed_images(dataset):
-    loader = torch.utils.data.DataLoader(dataset, batch_size=24, shuffle=True)
-    batch = next(iter(loader))
-    images, labels = batch
+#
+# # To show the images loaded from the ImageFolder above
+# def show_transformed_images(dataset):
+#     loader = torch.utils.data.DataLoader(dataset, batch_size=24, shuffle=True)
+#     batch = next(iter(loader))
+#     images, labels = batch
+#
+#     # the grid nrow determines the images per row
+#     grid = torchvision.utils.make_grid(images, nrow=4)
+#     # figure size is the size of the display image
+#     plt.figure(figsize=(5, 5))
+#     plt.imshow(np.transpose(grid, (1, 2, 0)))
+#     plt.show()
+#     print('labels: ', labels)
+#
+#
+# show_transformed_images(train_dataset)
 
-    # the grid nrow determines the images per row
-    grid = torchvision.utils.make_grid(images, nrow=4)
-    # figure size is the size of the display image
-    plt.figure(figsize=(5, 5))
-    plt.imshow(np.transpose(grid, (1, 2, 0)))
-    plt.show()
-    print('labels: ', labels)
-
-
-show_transformed_images(train_dataset)
-'''
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
 original_loader = torch.utils.data.DataLoader(original_dataset, batch_size=32, shuffle=True)
@@ -54,10 +54,9 @@ def set_device():
 
 
 def train_nn(model, train_loader, test_loader, criterion, optimizer, n_epochs):
-    device = set_device()
 
     for epoch in range(n_epochs):
-        print("Epoch number %d" %(epoch+1))
+        print("Epoch number %d" % (epoch + 1))
         model.train()
         running_loss = 0.0
         running_correct = 0.0
@@ -79,13 +78,13 @@ def train_nn(model, train_loader, test_loader, criterion, optimizer, n_epochs):
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-            running_correct += (labels==predicted).sum().item()
+            running_correct += (labels == predicted).sum().item()
 
-        epoch_loss = running_loss/len(train_loader)
+        epoch_loss = running_loss / len(train_loader)
         epoch_acc = 100 * running_correct / total
 
-        print("- Testing dataset. Got %d out of %d images correctly (%.3f%%). Epoch loss: %.3f"
-              %(running_correct, total, epoch_acc, epoch_loss))
+        print("- Training dataset. Got %d out of %d images correctly (%.3f%%). Epoch loss: %.3f"
+              % (running_correct, total, epoch_acc, epoch_loss))
 
         evaluate_model_on_test_set(model, test_loader)
 
@@ -113,8 +112,8 @@ def evaluate_model_on_test_set(model, test_loader):
             predicted_correctly_on_epoch += (predicted == labels).sum().item()
 
     epoch_acc = 100.0 * predicted_correctly_on_epoch / total
-    print("- Training dataset. Got %d out of %d images correctly (%.3f%%)"
-              %(predicted_correctly_on_epoch, total, epoch_acc))
+    print("- Validation dataset. Got %d out of %d images correctly (%.3f%%)"
+          % (predicted_correctly_on_epoch, total, epoch_acc))
 
 
 resnet18_model = models.resnet18(pretrained=True)
